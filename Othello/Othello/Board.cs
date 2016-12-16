43488aeb493912ba5f1c3561ca7329b3a7095f52
cyril.ruedin <cyril.ruedin@he-arc.ch>
2016-12-16 09:44:29 +0100

    Class Board, 0.1%

---
+++ Othello/Othello/Board.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Othello
+{
+    class Board
+    {
+        public enum TileState
+        {
+            Empty,
+            Black,
+            White
+        };
+
+        private TileState[,] gameBoard;
+
+        public  Board()
+        {
+            gameBoard = new TileState[8,8];
+        }
+
+
+    }
+}
